f506b7b7914d221f4ee1fb08a5267a607ec425ed
ch-liuzhide <liuzhide.lzd@antgroup.com>
2025-01-22 19:04:47 +0800

    debug task

--- lambda_task_subscriber/handler.py
+++ lambda_task_subscriber/handler.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 
 MAX_RETRY_COUNT = 5
@@ -6,11 +7,16 @@ MAX_RETRY_COUNT = 5
 
 # 根据资源类型，处理不同的业务。
 def lambda_handler(event, context):
+    print(f"++event: {event}")
+    output_queue_url = os.getenv("OUTPUT_QUEUE_URL")
+    print(f"OUTPUT_QUEUE_URL: {output_queue_url}")
+    bucket_name = os.getenv("AWS_BUCKET_NAME")
+    print(f"AWS_BUCKET_NAME: {bucket_name}")
     if event:
         sqs_batch_response = {}
-        print(f"event: {event}")
         for record in event["Records"]:
             body = record["body"]
-            print(f"receive message here: {body}")
+            print(f"r++eceive message here: {body}")
             message_dict = json.loads(body)
+
         return sqs_batch_response
