31f6e65247cc4bf9a987c1e1610a962b997390fc
ch-liuzhide <liuzhide.lzd@antgroup.com>
2025-02-15 15:47:40 +0800

    perf(save_chunk_list): batch save chunks

--- lambda_task_subscriber/dao/chunk_dao.py
+++ lambda_task_subscriber/dao/chunk_dao.py
@@ -10,9 +10,12 @@ class ChunkDao(BaseDAO):
         self.CHUNK_TABLE_NAME = get_env_variable("CHUNK_TABLE_NAME", "chunk")
 
     def save_chunk_list(self, chunk_list: List[Chunk]):
-        self.client.table(self.CHUNK_TABLE_NAME).insert(
-            [
-                chunk.model_dump(exclude_unset=True, exclude_none=True)
-                for chunk in chunk_list
-            ]
-        ).execute()
+        chunk_size = 30
+        for i in range(0, len(chunk_list), chunk_size):
+            chunk_batch = chunk_list[i : i + chunk_size]
+            self.client.table(self.CHUNK_TABLE_NAME).insert(
+                [
+                    chunk.model_dump(exclude_unset=True, exclude_none=True)
+                    for chunk in chunk_batch
+                ]
+            ).execute()
