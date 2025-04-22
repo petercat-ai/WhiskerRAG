47b1d924561cca404cbce4f1c555946c99a72782
kunqi <kunqi.fc@digital-engine.com>
2025-04-22 09:51:35 +0800

    feat: sync chunk API handler

--- server/api/chunk/router.py
+++ server/api/chunk/router.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from core.auth import get_tenant
 from core.plugin_manager import PluginManager
 from core.response import ResponseModel
@@ -19,3 +21,50 @@ async def get_chunk_list(
     db_engine = PluginManager().dbPlugin
     chunks: PageResponse[Chunk] = await db_engine.get_chunk_list(tenant.tenant_id, body)
     return ResponseModel(data=chunks, success=True)
+
+
+@router.delete(
+    "/id/{id}/model_name/{model_name}",
+    operation_id="delete_chunk_by_id",
+    response_model_by_alias=False,
+)
+async def delete_chunk_by_id(
+    id: str, model_name: str, tenant: Tenant = Depends(get_tenant)
+) -> ResponseModel[object]:
+    db_engine = PluginManager().dbPlugin
+    chunks: PageResponse[Chunk] = await db_engine.delete_chunk_by_id(
+        tenant.tenant_id, id, model_name
+    )
+    return ResponseModel(data=chunks, success=True)
+
+
+@router.get(
+    "/id/{id}/model_name/{model_name}",
+    operation_id="get_chunk_by_id",
+    response_model_by_alias=False,
+)
+async def get_chunk_by_id(
+    id: str, model_name: str, tenant: Tenant = Depends(get_tenant)
+) -> ResponseModel[PageResponse[Chunk]]:
+    db_engine = PluginManager().dbPlugin
+    chunks: PageResponse[Chunk] = await db_engine.get_chunk_by_id(
+        tenant.tenant_id, id, model_name
+    )
+    return ResponseModel(data=chunks, success=True)
+
+
+@router.post("/save", operation_id="save_chunk", response_model_by_alias=False)
+async def save_chunk(
+    body: Chunk, tenant: Tenant = Depends(get_tenant)
+) -> ResponseModel[PageResponse[Chunk]]:
+    db_engine = PluginManager().dbPlugin
+    # 如果传入chunkId则为修改
+    if body.chunk_id:
+        chunk = await db_engine.get_chunk_by_id(
+            tenant.tenant_id, body.chunk_id, body.embedding_model_name
+        )
+        for field_name in body.keys():
+            if getattr(body, field_name) is None:
+                setattr(body, field_name, getattr(chunk, field_name))
+    chunks: List[Chunk] = await db_engine.save_chunk_list([body], body)
+    return ResponseModel(data=chunks[0], success=True)
