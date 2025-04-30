15ad55209701c9c01c39050bbc6dae108e4e7cfb
gakkiyomi <gakkiyomi@aliyun.com>
2025-04-30 11:44:08 +0800

    test: add test case for retrieval_counter (#29)
    * test: add test case for retrieval_counter
    
    * refactor(retrieve): add retrieve router
    
    ---------
    
    Co-authored-by: kunqi <kunqi.fc@digital-engine.com>
    Co-authored-by: ch-liuzhide <liuzhide.lzd@antgroup.com>

--- server/api/retrieval/router.py
+++ server/api/retrieval/router.py
@@ -1,9 +1,15 @@
 from typing import List
 
+from deprecated import deprecated
+
 from core.auth import get_tenant
 from core.plugin_manager import PluginManager
 from core.response import ResponseModel
-from core.retrieval_counter import retrieval_counter
+from core.retrieval_counter import (
+    RetrievalCounter,
+    get_retrieval_counter,
+    retrieval_count,
+)
 from fastapi import APIRouter, Depends
 from whiskerrag_types.model import (
     RetrievalByKnowledgeRequest,
@@ -11,6 +17,7 @@ from whiskerrag_types.model import (
     RetrievalChunk,
     Tenant,
 )
+from whiskerrag_types.model.retrieval import RetrievalRequest
 
 router = APIRouter(
     prefix="/api/retrieval",
@@ -28,27 +35,45 @@ router = APIRouter(
 async def retrieve_knowledge_content(
     body: RetrievalByKnowledgeRequest,
     tenant: Tenant = Depends(get_tenant),
+    counter: RetrievalCounter = Depends(get_retrieval_counter),
 ) -> ResponseModel[List[RetrievalChunk]]:
     """
     Retrieve certain chunks within a knowledge_id, for example, within a specific PDF file.
     """
     db_engine = PluginManager().dbPlugin
     res = await db_engine.search_knowledge_chunk_list(tenant.tenant_id, body)
-    retrieval_counter(res)
+    retrieval_count(counter, res)
     return ResponseModel(success=True, data=res)
 
 
+@deprecated("retrieve_space_content is deprecated, please use retrieve instead.")
 @router.post(
     "/space", operation_id="retrieve_space_content", response_model_by_alias=False
 )
 async def retrieve_space_content(
     body: RetrievalBySpaceRequest,
     tenant: Tenant = Depends(get_tenant),
+    counter: RetrievalCounter = Depends(get_retrieval_counter),
 ) -> ResponseModel[List[RetrievalChunk]]:
     """
     Retrieve chunks within a space_id, for example, given a petercat bot_id, retrieve all chunks under that bot_id.
     """
     db_engine = PluginManager().dbPlugin
     res = await db_engine.search_space_chunk_list(tenant.tenant_id, body)
-    retrieval_counter(res)
+    retrieval_count(counter, res)
+    return ResponseModel(success=True, data=res)
+
+
+@router.post("/", operation_id="retrieve", response_model_by_alias=False)
+async def retrieve(
+    body: RetrievalRequest,
+    tenant: Tenant = Depends(get_tenant),
+    counter: RetrievalCounter = Depends(get_retrieval_counter),
+) -> ResponseModel[List[RetrievalChunk]]:
+    """
+    Retrieve chunks within a space_id, for example, given a petercat bot_id, retrieve all chunks under that bot_id.
+    """
+    db_engine = PluginManager().dbPlugin
+    res = await db_engine.retrieve(tenant.tenant_id, body)
+    retrieval_count(counter, res)
     return ResponseModel(success=True, data=res)
