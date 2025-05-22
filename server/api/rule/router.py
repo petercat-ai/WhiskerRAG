50da4f4a40bfd55beb4d95d8ff4306ce8bdd9ae5
ch-liuzhide <liuzhide.lzd@antgroup.com>
2025-05-22 13:51:56 +0800

    fix(rule): fix rule ruturn

--- server/api/rule/router.py
+++ server/api/rule/router.py
@@ -3,7 +3,7 @@ from core.log import logger
 from core.plugin_manager import PluginManager
 from core.response import ResponseModel
 from fastapi import APIRouter, Depends, HTTPException, Path
-from whiskerrag_types.model import Tenant, GlobalRule
+from whiskerrag_types.model import Tenant
 
 router = APIRouter(
     prefix="/api/rule",
@@ -16,7 +16,7 @@ router = APIRouter(
 @router.get("/global", operation_id="get_global_rule", response_model_by_alias=False)
 async def get_global_rule(
     tenant: Tenant = Depends(get_tenant),
-) -> ResponseModel[GlobalRule]:
+) -> ResponseModel[str]:
     try:
         db_engine = PluginManager().dbPlugin
         rule = await db_engine.get_tenant_rule(tenant.tenant_id)
@@ -43,7 +43,7 @@ async def get_global_rule(
 async def get_space_rule(
     space_id: str = Path(..., description="knowledge base id"),
     tenant: Tenant = Depends(get_tenant),
-) -> ResponseModel[GlobalRule]:
+) -> ResponseModel[str]:
     try:
         db_engine = PluginManager().dbPlugin
         rule = await db_engine.get_space_rule(tenant.tenant_id, space_id)
