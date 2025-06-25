49c0b534cf3d7e81785c81d1932fe494e73e1d0a
ch-liuzhide <liuzhide.lzd@antgroup.com>
2025-06-25 21:23:03 +0800

    feat(dashboard): add system dashboard

---
+++ server/api/dashboard/router.py
@@ -0,0 +1,67 @@
+from importlib.resources import Resource
+from typing import List, Optional
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+from core.plugin_manager import PluginManager
+from core.response import ResponseModel
+from whiskerrag_types.model import Tenant
+from core.auth import Action, Resource, get_tenant_with_permissions
+
+router = APIRouter(
+    prefix="/api/v1/dashboard",
+    tags=["dashboard"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+class GlobalInfo(BaseModel):
+    space_count: Optional[int] = None
+    knowledge_count: Optional[int] = None
+    task_count: Optional[int] = None
+    agent_count: Optional[int] = None
+    task_count: Optional[int] = None
+    last_1d_user_count: Optional[int] = None
+    last_7d_user_count: Optional[int] = None
+    sum_user_count: Optional[int] = None
+    tenant_count: Optional[int] = None
+    # system knowledge storage size
+    storage_size: Optional[str] = None
+
+
+class TenantLog(BaseModel):
+    content: str
+
+
+class TenantLogQuery(BaseModel):
+    page: int
+    page_size: int
+    start_time: Optional[str] = None
+    end_time: Optional[str] = None
+
+
+@router.get(
+    "/global_info", operation_id="get_global_info", response_model_by_alias=False
+)
+async def get_system_global_info() -> ResponseModel[GlobalInfo]:
+    db_engine = PluginManager().dbPlugin
+    system_info = await db_engine.get_system_info()
+    return ResponseModel(
+        success=True,
+        data=system_info,
+        message="Success",
+    )
+
+
+@router.get("/tenant_log", operation_id="get_tenant_log", response_model_by_alias=False)
+async def get_tenant_log(
+    body: TenantLogQuery,
+    tenant: Tenant = get_tenant_with_permissions(Resource.TENANT, [Action.READ]),
+) -> ResponseModel[List[TenantLog]]:
+    db_engine = PluginManager().dbPlugin
+    tenant_log = await db_engine.get_tenant_log(body, tenant.tenant_id)
+    return ResponseModel(
+        success=True,
+        data=tenant_log,
+        message="Success",
+    )
